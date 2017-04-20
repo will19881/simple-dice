51bc95212d0408ff36ce26212b68d5d6376f35e0
will19881 <shirunlin@gmail.com>
2017-04-19 22:18:56 -0230

    Update DataProvider.swift

--- basic/basic/DataProvider.swift
+++ basic/basic/DataProvider.swift
@@ -10,8 +10,6 @@ import Foundation
 import GameKit
 
 struct DataProvider{
-    let details = ["iphone 6","macbook pro 15->!!!", "oh my God there are so many","wala","this is the last one"]
-    
     func randomValue () -> Int {
 
         return GKRandomSource.sharedRandom().nextInt(upperBound: 6) + 1
